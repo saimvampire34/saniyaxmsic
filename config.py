c5703ced60064787006fb5ef6480f9359bfcfc41
MISHRAJII123 <ccaddnew@gmail.com>
2024-12-07 19:48:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MISHRAJII123/Applemusicop",
+    "https://github.com/MISHRAJII123/RADHAXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
