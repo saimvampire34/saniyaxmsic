0789361daac40afb4004ed2b815077f80f44b0af
saimvampire34 <saimvampire34@gmail.com>
2024-12-25 20:03:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/wl2nxn.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/fbjckz.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/wl2nxn.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/fbjckz.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
